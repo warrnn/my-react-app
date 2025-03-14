33f54dc96fe5da95611cab3e5c03bf81582e8a9d
Bryan Mogens Warren <bryanwarren86@gmail.com>
2025-03-14 22:04:31 +0700

    Add Article component and update App to display articles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
+import Article from "./components/Article";
 
 function App() {
-	const element = <h2>Hello, JSX!</h2>;
-
 	return (
-		<div style={{ textAlign: "center", padding: "20px" }}>
-			<Header title="Welcome to My App" />
-			{element}
-			<p>My first React app!</p>
-		</div>
+    <div className="App">
+      <Article name="Aa" titles={["hai", "hi", "he"]} />
+      <br></br>
+      <Article name="Nn" titles={["ho", "hei", "hu"]} />
+    </div>
 	);
 }
 
---
+++ src/components/Article.jsx
@@ -0,0 +1,14 @@
+function Article(props) {
+	return (
+		<>
+			<div>{props.name}</div>
+			<div>
+				{props.titles.map((title) => {
+					return <div>{title}</div>;
+				})}
+			</div>
+		</>
+	);
+}
+
+export default Article;
